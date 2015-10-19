36e5e90936c9c73cd33833e70ba918c580306b93
guiu.galdino <guiu.galdino@gmail.com>
2015-10-19 12:46:44 -0200

    teste

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -84,7 +84,7 @@ namespace WebApplication1.Controllers
                 return View(model);
             }
 
-            // This doesn't count login failures towards account lockout
+            // This doesn't count login fadsadilures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
