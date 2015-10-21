2917c08ac825c4abe04b8379ce56d3cdc4b6e09c
Ana Carolina <anacgp91@gmail.com>
2015-10-20 23:13:10 -0200

    resolvendo problema do css e adicionando modal de detalhes

---
+++ WebApplication1/Controllers/ReceitaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ReceitaController : Controller
+    {
+        public ActionResult Receita()
+        {
+            return View();
+        }
+    }
+}
