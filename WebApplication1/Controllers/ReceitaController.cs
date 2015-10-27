49aba093e116395262a6b2d22127e93763fe3a79
Ana Carolina <anacgp91@gmail.com>
2015-10-27 16:10:55 -0200

    lista de receitas favoritas

--- WebApplication1/Controllers/ReceitaController.cs
+++ WebApplication1/Controllers/ReceitaController.cs
@@ -12,5 +12,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+
+        public ActionResult Lista()
+        {
+            return View();
+        }
     }
 }
